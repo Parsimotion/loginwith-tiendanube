81ebcf88cc4fff819733eff191a2654e0056e5e8
ivogimenez <sgimenezivan@gmail.com>
2025-10-24 12:52:30 +0000

    fix: remove hardcoded secret string

--- server/config/tiendanubeStrategy.js
+++ server/config/tiendanubeStrategy.js
@@ -5,7 +5,7 @@ var TiendaNubeStrategy = require("passport-tiendanube").Strategy;
 
 passport.use(new TiendaNubeStrategy({
   clientID: 181,
-  clientSecret: "NO_HARDCODED_SECRET_HERE",
+  clientSecret: process.env.TIENDANUBE_CLIENTSECRET,
   userAgent: 'Producteca (andres@parsimotion.com)'
 }, function(accessToken, refreshToken, profile, done) {
   console.log(profile);
